b488f980c35865e2db1f294d90aaeee01685c4c7
praveendevopsk8s <praveendevopsk8s@gmail.com>
2024-10-06 07:41:51 +0530

    adding

---
+++ build/precache-manifest.1a213a8e3a250e48ff2cb7306b8219fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67097251d8d18bf98b61cdc6f2b90a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43f76c0f4270783bd15f",
+    "url": "/static/css/main.c2aca039.chunk.css"
+  },
+  {
+    "revision": "e1739488586cf2300338",
+    "url": "/static/js/2.86e82c67.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "43f76c0f4270783bd15f",
+    "url": "/static/js/main.22007ca9.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "564b42f150e36ffb79be330bdbbc90eb",
+    "url": "/static/media/logo.564b42f1.svg"
+  }
+]);
